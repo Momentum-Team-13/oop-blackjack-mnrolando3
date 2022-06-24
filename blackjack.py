82c2974f47684abe330e1d02bc21baa8ce5c08da
Metta Rolando <mnrolando3@gmail.com>
2022-06-24 10:29:04 -0400

    Super lost now. Just getting errors. Going to revert code to the version that printed cards and was less complicated and messy. Then take a break to relax.

--- blackjack.py
+++ blackjack.py
@@ -1,13 +1,13 @@
 # Write your blackjack game here.
 
 class Card():
-    def __init__(self, suit, value, rank):
+    def __init__(self):
         # card rank (face/number)
-        self.rank = rank
+        self.rank = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K']
         # card suit
-        self.suit = suit
+        self.suit = ['♠️', '♣️', '♥️', '♦️']
         # card point value
-        self.value = value
+        self.value = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
 
     def print_cards(self):
         print(f'The {self.rank} of {self.suit} is {self.value} points.')
@@ -34,6 +34,7 @@ class Deck():
         self.cards = []
         self.add_cards()
         print(f'deck? {self.cards}')
+        pass
 
     # def __str__(self):
     #     # for card in self.cards:
@@ -45,18 +46,20 @@ class Deck():
         for card in self.cards:
             card.print_cards()
             # print(self.cards)
-    #         print('print length of list', len(self.cards))
-    #         return f"this is our deck {self.cards}"
-    #     # create cards and add to list
+            # print('print length of list', len(self.cards))
+            # return f"this is our deck {self.cards}"
+        # create cards and add to list
 
-    def add_cards(self):
-        for suit_type in ['♠️', '♣️', '♥️', '♦️']:
-            for value_type in [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]:
-                for rank_type in ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K']:
-                    # name = f"{rank} {suit_type}"
-                    # name = Card(suit_type, value_type, rank_type)
-                    self.cards.append(Card(suit_type, value_type, rank_type))
-                    return f"this adds cards {self.cards}"
+    # def add_cards(self):
+    #     for suit_type in suit:
+    #         for value_type in value:
+    #             for rank_type in rank:
+    #                 # name = f"{rank} {suit_type}"
+    #                 # name = Card(suit_type, value_type, rank_type)
+    #                 # print(name)
+    #                 # deck.add_cards(name)
+    #                 self.cards.append(Card(suit_type, value_type, rank_type))
+    #                 return f"this adds cards {self.cards}"
 
     # random function. random.choice
     def shuffle_cards():
@@ -73,19 +76,20 @@ class Deck():
 # that is right, didn't think about that...
 
 
-deck = Deck()
-deck.add_cards()
-deck.print_cards()
-# suit = ['♠️', '♣️', '♥️', '♦️']
-# value = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
-# rank = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K']
-# for suit_type in suit:
-#     for index in range(len(value)):
-#         value_type = value[index]
-#         rank_type = rank[index]
-#         name = f"{rank} {suit_type}"
-#         name = Card(suit_type, value_type, rank_type)
-#         # print(name)
-#         deck.add_cards(name)
-
+card = Card()
+card.print_cards()
+print(card)
+# deck.print_cards()
+# deck.add_cards()
+# print(deck)
+suit = ['♠️', '♣️', '♥️', '♦️']
+value = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
+rank = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K']
+for suit_type in suit:
+    for value_type in value:
+        for rank_type in rank:
+            name = f"{rank} {suit_type}"
+            name = Card()
+        # print(name)
+        # deck.add_cards(name)
 # print("deck.print_cards", deck.add_cards(name))
