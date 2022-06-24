6749f45f4e26aeff7d1d7c39aadfd79b5dbb3806
Metta Rolando <mnrolando3@gmail.com>
2022-06-23 21:38:47 -0400

    Isolated deck and cards to work on building deck from card objects

--- blackjack.py
+++ blackjack.py
@@ -1,26 +1,5 @@
 # Write your blackjack game here.
 
-# for player
-class Player():
-    def __init__(self):
-        self.player_hand = []
-        pass
-    # right? that makes sense? Yes i believe so
-
-    # ask for another card
-    def hit_player(self):
-        pass
-
-    # calculate score
-    def player_score():
-        pass
-
-    # this feels spicy -Diego
-    # betting is in our "extras" haha
-    def place_wager():
-        pass
-
-
 class Deck():
     def __init__(self):
         self.cards = []
@@ -39,9 +18,8 @@ class Deck():
     #     # create cards and add to list
 
     def add_cards(self, card):
-        # for card in self.cards:
-        #     self.cards.append(card)
-        #     print('print length of list', len(self.cards))
+        self.cards.append(card)
+        print('print length of list', len(self.cards))
         return f"this adds cards {self.cards}"
 
     # random function. random.choice
@@ -83,37 +61,6 @@ class Card():
     #             return name
     #             deck.add_cards(name)
 
-
-# is this redundant? no because the Dealer needs 
-# to start the deal and has different rules than a player 
-# (loop will stop at 17, etc)
-class Dealer():
-    def __init__(self):
-        self.dealer_hand = []
-        pass
-
-    # randomly selects 2 cards from Deck and adds to player_hand
-    # 2 cards selected are removed from Deck
-    # randomly selects 2 cards from Deck and adds to dealer_hand
-    # 2 cards selected are removed from Deck
-    def start_deal(self):
-        pass
-
-    # while loop as long as sum of dealer_hand < 17
-    # randomlly select 1 card from Deck and add to dealer_hand
-    # card selected is removed from Deck
-    # when sum of dealer_hand >= 17, stop
-    def hit_dealer(self):
-        pass
-
-    def dealer_score():
-        pass
-
-
-# other methods:
-# Ace (11 vs 1 value)
-# do we want the Ace value as a method for the Dealer/Player classes 
-# or for the Card class?
 
 deck = Deck()
 suit = ['♠️', '♣️', '♥️', '♦️']
