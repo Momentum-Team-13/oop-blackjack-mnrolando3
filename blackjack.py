6cfd068878d49fd16174e78d45747dc43ff6ba66
Metta Rolando <mnrolando3@gmail.com>
2022-06-26 13:54:31 -0400

    Cleaned up linting

--- blackjack.py
+++ blackjack.py
@@ -46,7 +46,7 @@ class Deck():
     #     # create cards and add to list
 
     def add_cards(self):
-        RANKS_VALUES = {'A':[1,11], '2':2, '3':3, '4':4, '5':5, '6':6, '7':7, '8':8, '9':9, '10':10, 'J':10, 'Q':10, 'K':10}
+        RANKS_VALUES = {'A': [1, 11], '2': 2, '3': 3, '4': 4, '5': 5, '6': 6, '7': 7, '8': 8, '9': 9, '10': 10, 'J': 10, 'Q': 10, 'K': 10}
         SUITS = ['♠️', '♣️', '♥️', '♦️']
 
         for rank in RANKS_VALUES:
@@ -60,6 +60,7 @@ class Deck():
     # random function. random.choice
     def shuffle_cards():
         pass
+
 
 deck = Deck()
 deck.add_cards()
